181bb2b1393139355d6bf3d3bf0c33eefe51032d
jonathandiaz <jonathan.diaz714@gmail.com>
2021-02-10 17:20:00 -0500

    started and got root component to work

---
+++ frontend/components/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route, Redirect, Switch, Link } from 'react-router-dom';
+
+const App = () => (
+    <div>
+        <header>
+            This is a header
+        </header>
+    </div>
+);
+
+export default App;
