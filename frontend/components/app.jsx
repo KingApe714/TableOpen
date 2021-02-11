1aa49ba2d47a5e2a11865b903027e67d8da5a79f
jonathandiaz <jonathan.diaz714@gmail.com>
2021-02-10 19:39:25 -0500

    got header to show link to head back to root

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Route, Redirect, Switch, Link } from 'react-router-dom';
+import { AuthRoute } from '../util/route_util';
+import SignUpFormContainer from './session_form/signup_form_container';
+import LoginFormContainer from './session_form/login_form_container';
 
 const App = () => (
     <div>
         <header>
+            <Link to='/'>
+                <h1>Let's head back to that young root page baby</h1>
+            </Link>
             This is a header
         </header>
-        
+        <Switch>
+            <AuthRoute exact path='/login' component={LoginFormContainer} />
+            <AuthRoute exact path="/signup" component={SignUpFormContainer} />
+        </Switch>
     </div>
 );
 
