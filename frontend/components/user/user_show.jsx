90701899e52efe1f3307737ddc8db4a8227e483f
jonathandiaz <jonathan.diaz714@gmail.com>
2021-04-01 18:51:45 -0400

    user show page with reservation index

--- frontend/components/user/user_show.jsx
+++ frontend/components/user/user_show.jsx
@@ -13,19 +13,23 @@ class UserShow extends React.Component {
     render() {
         const restaurants = this.props.restaurants;
         const reservations = this.props.reservations.map(reservation => (
-            <Link 
-                key={reservation.id}
-                id="reservation-link"
-                to={`/restaurants/${reservation.restaurant_id}`}>
+            <div>
+                <Link 
+                    key={reservation.id}
+                    id="reservation-link"
+                    to={`/restaurants/${reservation.restaurant_id}`}>
+                        <img src={restaurants[reservation.restaurant_id].photoUrl}></img>
+                </Link>
+                <div>
                     <div>
                         {restaurants[reservation.restaurant_id].name}
                         {reservation.reservation_date_time}
-                        <div>
-                            {reservation.guest_count}
-                        </div>
-                        <img src={restaurants[reservation.restaurant_id].photoUrl}></img>
                     </div>
-            </Link>
+                    <p>
+                        Table for {reservation.guest_count} {reservation.guest_count === 1 ? <>person</> : <>people</>}
+                    </p>
+                </div>
+            </div>
         ))
         return (
             <div>
