e7098ebdd1c8e8d9d8bd7a0b89e20fd44072b81d
jonathandiaz <jonathan.diaz714@gmail.com>
2021-04-22 22:34:15 -0400

    trie is rendering properly on the frontend

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -4,6 +4,9 @@ import TrieTree from '../../util/util_functions'
 class Search extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            searchTerm: ''
+        }
     }
 
     componentDidMount() {
@@ -11,17 +14,37 @@ class Search extends React.Component {
         this.props.fetchRestaurants()
     }
 
+    update(field) {
+        return e => this.setState({
+            [field]: e.currentTarget.value
+        })
+    }
+
     render() {
         //I've now set up a Trie Tree with all of the restaurant names
-        let trie = new TrieTree(this.props.restaurants.map(restaurant => {
-            return restaurant.name
-        }))
-        console.log(trie)
+        let trie;
+        if (this.props.restaurants.length > 0) {
+            trie = new TrieTree(this.props.restaurants.map(restaurant => {
+                return restaurant.name
+            }))
+            // console.log(trie.rootNode.filterWords(trie.rootNode, 'S'))
+            // console.log(trie.rootNode.filterWords(trie.rootNode, 'U'))
+            // console.log(trie.rootNode.filterWords(trie.rootNode, 'Unc'))
+        } else {
+            return null
+        }
+        const restaurantNames = trie.rootNode.filterWords(trie.rootNode, this.state.searchTerm);
+
+
+        // console.log(this.state.searchTerm)
+        // debugger
         return (
             <div>
                 <input  type="text"
+                        value={this.state.searchTerm}
+                        onChange={this.update('searchTerm')}
                         placeholder="Location, Restaurant, or Cuisine"/>
-                {/* {restaurantNames} */}
+                {restaurantNames}
             </div>
         )
     }
