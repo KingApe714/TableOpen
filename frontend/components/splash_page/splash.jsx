8574372ff0b26a23e9bc64be93d6cd93cece7634
jonathandiaz <jonathan.diaz714@gmail.com>
2021-04-28 04:10:59 -0400

    style search container on splash

--- frontend/components/splash_page/splash.jsx
+++ frontend/components/splash_page/splash.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import RestaurantIndexContainer from '../restaurant/restaurant_index_container';
+import SearchContainer from '../search/search_container';
 
 class Splash extends React.Component {
     constructor(props) {
@@ -10,7 +11,12 @@ class Splash extends React.Component {
         return (
             <div>
                 <div className="splash">
-                    Find your table for any occasion
+                    <div className="splash-inner">
+                        <div className="splash-title">
+                            Find your table for any occasion
+                        </div>
+                        <SearchContainer />
+                    </div>
                 </div>
                 <RestaurantIndexContainer/>
             </div>
