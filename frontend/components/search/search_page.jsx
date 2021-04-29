23e8139d097edcc7f06fde562dbf4e0a174dd59b
jonathandiaz <jonathan.diaz714@gmail.com>
2021-04-29 07:09:48 -0400

    search page testing

--- frontend/components/search/search_page.jsx
+++ frontend/components/search/search_page.jsx
@@ -3,20 +3,51 @@ import React from 'react';
 class SearchPage extends React.Component {
     constructor(props) {
         super(props)
-    }
-
-    componentDidMount() {
-        if (this.props.location.state.keyWord) {
-            this.props.searchRestaurants(this.props.location.state.keyWord)
-        } else {
-            this.props.searchRestaurants(this.props.location.state.searchTerm)
+        this.state = {
+            info: null
         }
     }
 
+    componentDidMount() {
+        // if (this.props.location.state.keyWord) {
+        //     this.props.searchRestaurants(this.props.location.state.keyWord)
+        // } else {
+        //     this.props.searchRestaurants(this.props.location.state.searchTerm)
+        // }
+
+        // if (this.props.location.state) {
+            
+        // } else {
+        //     this.setState({
+        //         info: JSON.parse(localStorage.getItem('info'))
+        //     })
+        // }
+
+        // if (this.state.info) {
+        //     if (this.state.info.keyWord) {
+        //         this.props.searchRestaurants(this.state.info.keyWord)
+        //     } else {
+        //         this.props.searchRestaurants(this.state.info.searchTerm)
+        //     }
+        // }
+    }
+
     render() {
-        // const restaurants = this.props.location.state.restaurants
+        console.log(this.state)
+        let info
         // debugger
-        console.log(this.props.restaurants)
+        // if (this.props.location.state) {
+        //     info = this.props.location.state;
+        //     localStorage.setItem('info', JSON.stringify(info));
+        //     this.setState({
+        //         info: info
+        //     })
+        // } else {
+        //     info = JSON.parse(localStorage.getItem('info'));
+        //     this.setState({
+        //         info: info
+        //     })
+        // }
         return (
             <div>
                 Halaba!!
