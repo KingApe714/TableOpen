da2c7d80d54187cda5f200c2f9d31b7e9da64bfa
jonathandiaz <jonathan.diaz714@gmail.com>
2021-07-27 16:48:53 -0400

    set up review front end

---
+++ frontend/util/review_util.js
@@ -0,0 +1,38 @@
+import { $CombinedState } from "redux"
+
+export const fetchReviews = () => {
+    return $CombinedState.ajax({
+        method: 'GET',
+        url: 'api/reviews'
+    })
+}
+
+export const fetchReview = reviewId => {
+    return $CombinedState.ajax({
+        method: 'GET',
+        url: `api/reviews/${reviewId}`
+    })
+}
+
+export const createReview = review => {
+    return $.ajax({
+        method: 'POST',
+        url: 'api/resrvations',
+        data: {review}
+    })
+}
+
+export const updateReview = review => {
+    return $.ajax({
+        method: 'PATCH',
+        url: `api/resrvations/${review.id}`,
+        data: {review}
+    })
+}
+
+export const deleteReview = reviewId => {
+    return $.ajax({
+        method: 'DELETE',
+        url: `api/resrvations/${reviewId}`
+    })
+}
