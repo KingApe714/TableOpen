1e2a1d683227c7bd63b5f9fa1d1677ee21e64d7b
jonathandiaz <jonathan.diaz714@gmail.com>
2021-07-28 14:50:36 -0400

    frontend reviews

---
+++ frontend/actions/review_actions.js
@@ -0,0 +1,35 @@
+import * as ReviewAPIUtil from '../util/review_uti';
+
+export const RECEIVE_ALL_REVIEWS = 'RECEIVE_ALL_REVIEWS';
+export const RECEIVE_REVIEW = 'RECEIVE_REVIEW';
+export const REMOVE_REVIEW = 'REMOVE_REVIEW';
+
+const receiveAllReviews = (reviews) => ({
+    type: RECEIVE_ALL_REVIEWS,
+    reviews
+})
+
+const receiveReview = (review) => ({
+    type: RECEIVE_REVIEW,
+    review
+})
+
+const removeReview = (reviewId) => ({
+    type: REMOVE_REVIEW,
+    reviewId
+})
+
+export const fetchReviews = () => (
+    ReviewAPIUtil.fetchReviews()
+        .then(reviews => dispatchEvent(receiveAllReviews(reviews)))
+)
+
+export const createReview = (review) => (
+    ReviewAPIUtil.createReview(review)
+        .then(review => dispatchEvent(receiveReview(review)))
+)
+
+export const updateReview = (review) => dispatch => (
+    ReviewAPIUtil.updateReview(review)
+        .then(review => dispatch(receiveReview(review)))
+)
--- frontend/util/review_util.js
+++ frontend/util/review_util.js
@@ -1,5 +1,3 @@
-import { $CombinedState } from "redux"
-
 export const fetchReviews = () => {
     return $CombinedState.ajax({
         method: 'GET',
